259771cf248b06b13acbdb9caf28a7bee86d3fbc
LokendraPandey1 <160410746+LokendraPandey1@users.noreply.github.com>
2026-01-11 20:20:06 +0530

    remove files

--- src/ui/overlay.js
+++
@@ -1,7 +0,0 @@
-export const statusElem = document.getElementById('status-text');
-
-export function updateStatus(message) {
-    if (statusElem) {
-        statusElem.innerText = message;
-    }
-}
